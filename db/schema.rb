02aaac8f36cb24936cc4304015873722af0677f1
Yoshikauz Fukaya <richvote@gmail.com>
2020-05-01 13:11:50 +0900

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20200429113021) do
 
-  create_table "test", id: false, force: :cascade do |t|
-    t.text "C1"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
